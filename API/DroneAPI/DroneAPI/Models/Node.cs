484cad8c125d2e0641617baa5e6c499b52d2b889
Albert-David Bakker <albertdavidbakker@hotmail.com>
2016-11-21 10:45:46 +0100

    node

---
+++ API/DroneAPI/DroneAPI/Models/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DroneAPI.Models
+{
+    public class Node
+    {
+        int x;
+        int y;
+    }
+}
