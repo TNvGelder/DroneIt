dfe498f151127ef3fed870f3b1c739ff84ef14a9
TNvGelder <TNvGelder@gmail.com>
2016-11-21 11:36:39 +0100

    qualitycheckcontroller

---
+++ API/DroneAPI/DroneAPI/Controllers/QualityCheckController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DroneAPI.Controllers
+{
+    public class QualityCheckController : ApiController
+    {
+        // POST api//CheckProduct/5
+        public void CheckProduct(int productId)
+        {
+            
+        }
+    }
+}
