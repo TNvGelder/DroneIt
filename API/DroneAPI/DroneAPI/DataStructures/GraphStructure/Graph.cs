528849156d11d0a17b8b772e0aa92f3cdfa890ba
TNvGelder <TNvGelder@gmail.com>
2016-11-22 10:05:30 +0100

    Added some datastructures

---
+++ API/DroneAPI/DroneAPI/DataStructures/GraphStructure/Graph.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DroneAPI.Models;
+
+namespace DroneAPI.DataStructures.GraphStructure
+{
+    public class Graph
+    {
+        private Dictionary<Position, GraphNode> _nodeDictionary;
+        
+
+        public void Dijkstra(Position startPosition, Position endPosition)
+        {
+            //PriorityQueue<Path> pq = new PriorityQueue<Path>();
+            if (!_nodeDictionary.ContainsKey(startPosition))
+            {
+                foreach (GraphNode node in _nodeDictionary.Values)
+                {
+                    node.Reset();
+                }
+            }
+
+            int nodesSeen = 0;
+            //var heap = new C5.IntervalHeap<int>();
+            while (nodesSeen < _nodeDictionary.Count)
+            {
+                
+            }
+            
+        }
+
+        private GraphNode getNode(Position position)
+        {
+            if (_nodeDictionary.ContainsKey(position))
+            {
+                return _nodeDictionary[position];
+            }
+            else
+            {
+                GraphNode node = new GraphNode(position);
+                _nodeDictionary[position] = node;
+                return node;
+            }
+        }
+    }
+}
---
+++ API/DroneAPI/DroneAPI/DataStructures/GraphStructure/GraphNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DroneAPI.Models
+{
+    public class GraphNode
+    {
+        public Position Position { get; set; }
+        public List<GraphNode> Adjacent { get; set; }
+        public GraphNode Prev { get; set; }
+        public double Distance { get; set; }
+        private static double _unknownDist = -1;
+
+        public double Dist { get; set; }
+
+        public GraphNode(Position position)
+        {
+            this.Position = position;
+        }
+
+        public void Reset()
+        {
+            Distance = _unknownDist;
+            Prev = null;
+
+        }
+    }
+}
