520ac3e8466ef60eaf0c1ba48ef46c326ef9f90b
Gerhard Kroes <gerhardkroes@hotmail.com>
2016-11-23 16:39:16 +0100

    Turning drone actions
    Drone turns after every action

--- DroneNodeJS/drone.js
+++ DroneNodeJS/drone.js
@@ -11,10 +11,18 @@ var turnto = 0;
 app.listen(8000);
 console.log("listening on port 8000");
 
+function ActionDone(){
+	if(!turn){
+		setTimeout(function () {
+			console.log('Action done');
+			io.sockets.emit('done');
+		}, 5000); 
+	}
+}
+
 client.on('navdata', function(navdata) {
 	if(navdata.demo != null){
 		clockwiseDegrees = parseInt(navdata.demo.clockwiseDegrees);
-		console.log(navdata);
 	}
 	
 	if(turn){							
@@ -28,7 +36,7 @@ client.on('navdata', function(navdata) {
 		} else {
 			turn = false;
 			client.stop();
-			console.log("Turning done");
+			ActionDone();
 		}
 		console.log(clockwiseDegrees);
 	}
@@ -45,13 +53,15 @@ io.sockets.on('connection', function (socket) {
 			// Start
 			case "start":			
 				client
-					.after(10, function() {
-						this.takeoff();
-					})
 					.after(1000, function() {
 						this.calibrate(0);
+						console.log('Calibrating...');
 					})
-					.after(10000, function() {
+					.after(5000, function() {
+						this.takeoff();
+						console.log('Taking off');
+					})
+					.after(20000, function() {
 						north = clockwiseDegrees;
 						console.log('Calibrate done');
 						io.sockets.emit('done');
@@ -70,7 +80,6 @@ io.sockets.on('connection', function (socket) {
 			
 			// Turn
 			case "turn":
-				north = -60;
 				if(north >= 0){
 					param = (parseInt(param) + parseInt(north));
 				} else if(north < 0){
@@ -87,62 +96,70 @@ io.sockets.on('connection', function (socket) {
 			
 			// Forward
 			case "forward":
+				turnto = clockwiseDegrees;
 				client.front(0.2);
-				client.after(param * 1000, function() {
+				client.after(param * 625, function() {
 					this.stop();
+					turn = true;
 				});
 				
 				break;
 				
 			// Backward
 			case "backward":
+				turnto = clockwiseDegrees;
 				client.back(0.2);
-				client.after(param * 1000, function() {
+				client.after(param * 625, function() {
 					this.stop();
+					turn = true;
 				});
 				
 				break;
 				
 			// Left
 			case "left":
+				turnto = clockwiseDegrees;
 				client.left(0.2);
-				client.after(param * 1000, function() {
+				client.after(param * 625, function() {
 					this.stop();
+					turn = true;
 				});
 				
 				break;
 			
 			// Right
 			case "right":
+				turnto = clockwiseDegrees;
 				client.right(0.2);
-				client.after(param * 1000, function() {
+				client.after(param * 625, function() {
 					this.stop();
+					turn = true;
 				});
 				
 				break;
 				
 			// Rise
-			case "rise":			
+			case "rise":
+				turnto = clockwiseDegrees;
 				client.up(0.5);
 				client.after(param * 1000, function() {
 					this.stop();
-				});		
+					turn = true;
+				});
 				
 				break;
 				
 			// Fall
-			case "fall":			
+			case "fall":	
+				turnto = clockwiseDegrees;
 				client.down(0.5);
 				client.after(param * 1000, function() {
 					this.stop();
+					turn = true;
 				});		
 				
 				break;
 		}
-		
-		if(!turn){
-			console.log('Action done');
-			io.sockets.emit('done');
-		}
+		ActionDone();
 	});
 });
