dbdc1db8d52d1b7d8e088bd50e15bc95ecc32c58
HJLeusink <henkjan1994@gmail.com>
2016-11-23 13:00:04 +0100

    Commments toegevoegd in de Angular App.

--- Website/controllers/AddProductController.js
+++ Website/controllers/AddProductController.js
@@ -1,6 +1,7 @@
 app.controller("AddProductController", function ($scope, $http, $location) {
 	$scope.ProductName; 
 
+	//Saves a product by submitting it to the API
 	$scope.SaveProduct = function() {
 		$http.post( "http://localhost:62553/api/Products", { Name: $scope.ProductName })
 		.then(
@@ -13,6 +14,7 @@ app.controller("AddProductController", function ($scope, $http, $location) {
 		);
 	}
 
+	//Goes back to the overview page.
 	$scope.Cancel = function() {
 		$location.path("/");
 	}
--- Website/controllers/ProductOverviewController.js
+++ Website/controllers/ProductOverviewController.js
@@ -1,6 +1,7 @@
 app.controller("ProductOverviewController", function ($scope, $http, $location) {
     $scope.products = [];
 
+    //This methods loads the products from the API and puts them into the products array.
     $scope.LoadProducts = function() {
 		$http.get("http://localhost:62553/api/Products")
 		.then(
@@ -13,10 +14,12 @@ app.controller("ProductOverviewController", function ($scope, $http, $location) 
 	    );
 	}
 
+	//Go to the edit page.
 	$scope.EditProduct = function(Id) {
 		$location.path("/edit/" + Id);
 	}
 
+	//Delete a product after confirmation.
     $scope.DeleteProduct = function(Id) {
     	if(confirm("Weet u zeker dat u dit product wilt verwijderen?")) {
 	   		$http.delete("http://localhost:62553/api/Products/" + Id).then(function() {
