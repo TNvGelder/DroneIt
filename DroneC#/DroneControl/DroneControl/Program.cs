ac129996010799a575d7590eacae88e730b9ecde
Gerhard Kroes <gerhardkroes@hotmail.com>
2016-12-05 10:02:17 +0100

    Drone api and aansturing drone connection

--- DroneC#/DroneControl/DroneControl/Program.cs
+++ DroneC#/DroneControl/DroneControl/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using AR.Drone.Avionics;
+using AR.Drone.Avionics.Objectives;
+using AR.Drone.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace DroneControl {
     class Program {
         static void Main(string[] args) {
+            
+
+            Console.Read();
         }
     }
 }
